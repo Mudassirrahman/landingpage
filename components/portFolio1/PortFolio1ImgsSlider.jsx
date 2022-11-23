7293febd6aee995a4dfa5f4e68e1997df8eaf76d
mudassir rehman <mudassirrehman1989@gmail.com>
2022-11-24 02:05:00 +0800

    tring difrnt css

--- components/portFolio1/PortFolio1ImgsSlider.jsx
+++ components/portFolio1/PortFolio1ImgsSlider.jsx
@@ -9,35 +9,35 @@ import "swiper/css/effect-cards";
 
 import { EffectCards } from "swiper";
 
-
 function PortFolio1ImgsSlider() {
   return (
     <>
-    <Swiper
-      effect={"cards"}
-      grabCursor={true}
-      modules={[EffectCards , Autoplay, Pagination, Navigation]}
-      className="mySwiper"
-      spaceBetween={30}
+      <Swiper
+        effect={"cards"}
+        grabCursor={true}
+        modules={[EffectCards, Autoplay, Pagination, Navigation]}
+        className="mySwiper"
+        spaceBetween={30}
         centeredSlides={true}
         autoplay={{
           delay: 2500,
           disableOnInteraction: false,
+          
         }}
         pagination={{
           clickable: true,
         }}
         navigation={true}
-    >
-      <SwiperSlide>Slide 1</SwiperSlide>
-      <SwiperSlide>Slide 2</SwiperSlide>
-      <SwiperSlide>Slide 3</SwiperSlide>
-      <SwiperSlide>Slide 4</SwiperSlide>
-      <SwiperSlide>Slide 5</SwiperSlide>
-      <SwiperSlide>Slide 6</SwiperSlide>
-    </Swiper>
-  </>
-  )
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+      </Swiper>
+    </>
+  );
 }
 
 export default PortFolio1ImgsSlider;
