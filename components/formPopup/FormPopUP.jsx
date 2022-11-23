0859dd6bd49b0573f607ecd0def844f052179730
mudassir rehman <mudassirrehman1989@gmail.com>
2022-11-23 21:35:54 +0800

    popupForm css changed

--- components/formPopup/FormPopUP.jsx
+++ components/formPopup/FormPopUP.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import styles from "../../styles/Footer1.module.css"
+import styles from "../../styles/FormPopUP.module.css"
 
 function FormPopUP() {
   return (
     <>
       <div
-        className="modal fade custmWidth"
+        className={`${"modal rounder-0"} ${styles.modal}`}
         id="modalContactForm"
         tabindex="-1"
         role="dialog"
@@ -15,8 +15,8 @@ function FormPopUP() {
         <div className="modal-dialog" role="document">
           <div className="modal-content">
             <div className="modal-header border-0 text-center">
-              <h4 className="modal-title w-100 font-weight-bold">
-                Pitch Your Idea
+              <h4 className={`${"modal-title w-100"} ${styles.formHeading}`}>
+                Pitch Your Ideas
               </h4>
               <button
                 type="button"
@@ -28,27 +28,27 @@ function FormPopUP() {
               </button>
             </div>
             <div className="modal-body mx-3">
-              <div className="md-form mb-5">
+              <div className="md-form mb-3">
                 <input
                   type="text"
                   id="form34"
-                  className="form-control validate"
+                  className="form-control validate inputHeight"
                   placeholder="Your name"
                 />
               </div>
-              <div className="md-form mb-5">
+              <div className="md-form mb-3">
                 <input
                   type="email"
                   id="form29"
-                  className="form-control validate"
+                  className="form-control validate inputHeight"
                   placeholder="Email address"
                 />
               </div>
-              <div className="md-form mb-5">
+              <div className="md-form mb-3">
                 <input
                   type="text"
                   id="form32"
-                  className="form-control validate"
+                  className="form-control validate inputHeight"
                   placeholder="Subject line"
                 />
               </div>
@@ -56,15 +56,15 @@ function FormPopUP() {
                 <textarea
                   type="text"
                   id="form8"
-                  className="md-textarea form-control mb-2"
+                  className="md-textarea form-control msgHeight mb-1"
                   rows="4"
                   placeholder="Enter your message"
                 ></textarea>
-                <input type="file" name="" id="" className="form-control" />
+                {/* <input type="file" name="" id="" className="form-control" /> */}
               </div>
             </div>
-            <div className="modal-footer d-flex justify-content-center mx-3">
-              <button className={`${"btn w-100 px-5"} ${styles.btnbg1}`}>SUBMIT</button>
+            <div className="modal-footer d-flex justify-content-center mx-3 border-0">
+              <button className={`${"btn w-100 px-5"} ${styles.btnbg1} ${styles.formBtnHeight}`}>SUBMIT</button>
             </div>
           </div>
         </div>
