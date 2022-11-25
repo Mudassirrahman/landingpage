de6001742729385c81b1dc8b77cd2ebc092f97f9
mudassir rehman <mudassirrehman1989@gmail.com>
2022-11-25 17:41:25 +0500

    improving css matching with figma file

--- components/footer/Footer1.jsx
+++ components/footer/Footer1.jsx
@@ -6,12 +6,12 @@ function Footer1() {
     <div className={styles.marginTopFoo}>
     <div className={`" px-4 py-lg-5 py-md-5 py-sm-5 py-0 text-center" ${styles.footerbg} ${styles.footerPaddingTop} `}>
       <div className="py-5">
-        <div className="col-lg-6 py-lg-3 py-md-3 py-sm-3 py-0 mx-auto">
+        <div className="col-lg-6 py-lg-3 py-md-3 py-sm-1 py-0 mx-auto">
           <p className={`" px-4 mb-5 tColor" ${styles.footerHeading} ${styles.tColor} `}>
             Got an idea? I am open to meet creative minds and
             collaborations.{" "}
           </p>
-          <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
+          <div className={` ${"d-grid gap-2 d-sm-flex justify-content-sm-center mb-lg-0 mb-md-0 mb-sm-0"} ${styles.FooterBtn} `}>
             <button
               type="button"
               className={`${"btn btn-lg px-4"} ${styles.btnbg1}`}
